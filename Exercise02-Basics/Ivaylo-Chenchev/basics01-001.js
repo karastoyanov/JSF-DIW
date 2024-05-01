c8d4ca9e26cf81656ee5e696f79ff22ec012c6f2
Ivaylo Chenchev <ivaylo.chenchev@gmail.com>
2024-05-01 23:40:29 +0300

    Task basics01-001 - the solution.

---
+++ Exercise02-Basics/Ivaylo-Chenchev/basics01-001.js
@@ -0,0 +1,16 @@
+/* 
+
+BASICS01-001: Write ten correct identifiers, following the camelCase naming convention.
+
+*/
+
+01) firstName
+02) lastName
+03) tmpX
+04) tmpY
+05) tmpZ
+06) currentLocation 
+07) postCodeAsString
+08) angleOneInDegrees
+09) angleOneInRadians
+10) circleRadiusInMm
