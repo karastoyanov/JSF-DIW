1f65b7f369199d4676dd53f7c562e184c7608953
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-08 22:49:07 +0300

    solution of task 005

---
+++ Plamen-Dilkinski/Basics01-005.js
@@ -0,0 +1,9 @@
+//Declare five variables, assign some numbers and print them on the console 
+
+let x = 10;
+let y = 0.53564;
+let z = 1002144;
+let someVariable = 10.5453;
+let lastVariable = 15463.1
+
+console.log("Printing five variables: " +x, y, z, someVariable, lastVariable);
