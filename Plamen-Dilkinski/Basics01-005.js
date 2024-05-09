614c52be73e314384d642565c3a35dd7c2ab523c
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 22:35:13 +0300

    altering the file extensions

--- Plamen-Dilkinski/Basics01-005.js
+++ Plamen-Dilkinski/Basics01-005.js
@@ -4,6 +4,6 @@ let x = 10;
 let y = 0.53564;
 let z = 1002144;
 let someVariable = 10.5453;
-let lastVariable = 15463.1
+let lastVariable = 15463.1;
 
 console.log("Printing five variables: " +x, y, z, someVariable, lastVariable);
