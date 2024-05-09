9b26a57237fe9ea758f3d5aadbe20a787907d7e4
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 22:28:01 +0300

    solution of task Basics01-008

---
+++ Plamen-Dilkinski/Basics01-008.js
@@ -0,0 +1,14 @@
+//      Declare 10 variables, assign the numbers from 100 to 109 and print the last two odd numbers on the console
+
+let var1 = 100; 
+let var2 = 101; 
+let var3 = 102;
+let var4 = 103;
+let var5 = 104;
+let var6 = 105;
+let var7 = 106; 
+let var8 = 107;
+let var9 = 108;
+let var10 = 109;
+
+console.log("The last two odd numbers are: " +var8+";"+var10);
