2fbdc9435e8727267a694a9c9135301931d3ff05
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 22:17:34 +0300

    solution of task basics01-007

---
+++ Plamen-Dilkinski/Basics01-007.js
@@ -0,0 +1,14 @@
+//      Decleare 10 variables, assign the numbers from 1 to 10 and print the first three odd numbers on the console
+
+let var1 = 1; 
+let var2 = 2; 
+let var3 = 3;
+let var4 = 4;
+let var5 = 5;
+let var6 = 6;
+let var7 = 7; 
+let var8 = 8;
+let var9 = 9;
+let var10 = 10;
+
+console.log("The first three odd numbers from 1 to 10 are: " +var1+";" +var3+";" +var5);
