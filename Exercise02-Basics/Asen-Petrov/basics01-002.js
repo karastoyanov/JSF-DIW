11ef99ee688143cb1df041dab3327d0ccc630b61
ivaylochenchev <58465111+ivaylochenchev@users.noreply.github.com>
2024-05-09 23:37:23 +0300

    Update basics01-002.js
    Use either:
    const PI;   // if it is a constant and will not be changed
    or 
    let pi;    // a variable -> use camelCase naming convention

--- Exercise02-Basics/Asen-Petrov/basics01-002.js
+++ Exercise02-Basics/Asen-Petrov/basics01-002.js
@@ -2,7 +2,13 @@ let sideA;
 let sideB;
 let sideC;
 
-let PI;
+// let PI;
+
+// if you want to define a constant -> use the name PI -> const PI;
+// in case, you define a variable -> more convenient naming convention is the camelCase 
+
+let pi;
+
 let r;
 
 let squareArea;
