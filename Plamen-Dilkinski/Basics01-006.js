e199983554664945c1a551c16138a49228fcaa79
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 17:13:36 +0300

    solution of task 6

---
+++ Plamen-Dilkinski/Basics01-006.js
@@ -0,0 +1,14 @@
+//      Decleare 10 variables, assign the numbers from 1 to 10 and print the even numbers on the console
+
+let var1 = 1; 
+let var2 = 2; 
+let var3 = 3;
+let var4 = 4;
+let var5 = 5;
+let var6 = 6;
+let var7 = 7; 
+let var8 = 8;
+let var9 = 9;
+let var10 = 10;
+
+console.log("The even numbers from var1 to var10 are: " +var2,var4,var6,var8,var10);
