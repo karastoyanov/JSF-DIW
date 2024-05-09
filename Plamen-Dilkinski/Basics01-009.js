947b068ed4935fe9a397cfd4c7ae540e38a74d02
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 22:57:21 +0300

    solution of task Basics01-009.js

---
+++ Plamen-Dilkinski/Basics01-009.js
@@ -0,0 +1,9 @@
+//  Declare five variables, assign the first five prime numbers and print them on the console
+
+let varPrime1 = 1;
+let varPrime2 = 3;
+let varPrime3 = 5;
+let varPrime4 = 7;
+let varPrime5 = 9;
+
+ console.log("The first five prime numbers: " +varPrime1+", "+varPrime2+", "+varPrime3+", "+varPrime4+", "+varPrime5);
