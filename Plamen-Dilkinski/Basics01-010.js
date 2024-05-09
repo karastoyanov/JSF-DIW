5377cf1cec51bb07461a16eb663e4ba0308c683d
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 23:26:00 +0300

    solution to last task from basics01

--- Plamen-Dilkinski/Basics01-010.js
+++ Plamen-Dilkinski/Basics01-010.js
@@ -3,18 +3,18 @@
 */
 
 //      5 variables where the first five primer numbers are assigned
-let varPrime1 = 1;
+let varPrime1 = 2;
 let varPrime2 = 3;
 let varPrime3 = 5;
 let varPrime4 = 7;
-let varPrime5 = 9;
+let varPrime5 = 11;
 
 // variables with first fiew prime numbers multipled by 3
 
-let varMultiply = 3;
+let varMultiply = 6;
 let varMultiply2 = 9;
 let varMultiply3 = 15;
 let varMultiply4 = 21;
-let varMultiply5 = 27;
+let varMultiply5 = 33;
 
 console.log("Printing all variables: "+ +varPrime1+", "+varPrime2+", "+varPrime3+", "+varPrime4+", "+varPrime5+", " +varMultiply+", "+varMultiply2+", " +varMultiply3+", "+varMultiply4+", "+varMultiply5);
---
+++ Plamen-Dilkinski/Basics01-011.js
@@ -0,0 +1,14 @@
+//      Declare ten variables. Assign them the first ten prime numbers. Print the numbers in reverse order.
+
+let varPrime1 = 2;
+let varPrime2 = 3;
+let varPrime3 = 5;
+let varPrime4 = 7;
+let varPrime5 = 9;
+let varPrime6 = 11;
+let varPrime7 = 13;
+let varPrime8 = 17;
+let varPrime9 = 19;
+let varPrime10 = 23;
+
+console.log("Prime numbers printed in reverse order "+varPrime10+", "+varPrime9+", "+varPrime8+", "+varPrime7+", "+varPrime6+", " +varPrime5+", "+varPrime4+", " +varPrime3+", "+varPrime2+", "+varPrime1);
