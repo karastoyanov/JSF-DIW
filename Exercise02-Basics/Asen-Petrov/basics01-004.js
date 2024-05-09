f00f00038038c26690d17a61c74fbca86d70f2c1
Asen Petrov <106155007+asenpetrov@users.noreply.github.com>
2024-05-10 00:00:52 +0300

    reworked task 4

--- Exercise02-Basics/Asen-Petrov/basics01-004.js
+++ Exercise02-Basics/Asen-Petrov/basics01-004.js
@@ -1,17 +1,36 @@
-let chars = ['a', 'b', 'c', 'd', 'f'];
-for (let index = 0; index < chars.length; index++) {
-    console.log(chars[index])
-    
-}
+let alphChar1 = 'a';
+let alphChar2 = 'b';
+let alphChar3 = 'c';
+let alphChar4 = 'd';
+let alphChar5 = 'f';
 
-let numbers = [123, 44444, 11231243, 21312312312, 0];
-for (let index = 0; index < chars.length; index++) {
-    console.log(numbers[index])
-    
-}
+console.log(alphChar1);
+console.log(alphChar2);
+console.log(alphChar3);
+console.log(alphChar4);
+console.log(alphChar5);
 
-let punctCharArr = ["/", "?", "!", ".", ","];
-for (let index = 0; index < chars.length; index++) {
-    console.log(punctCharArr[index])
-    
-}
+let num1 = 123;
+let num2 = 456;
+let num3 = 789;
+let num4 = 222;
+let num5 = 555;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+
+let punctCharArr1 = "/";
+let punctCharArr2 = "!";
+let punctCharArr3 = "?";
+let punctCharArr4 = ",";
+let punctCharArr5 = ".";
+
+console.log(punctCharArr1);
+console.log(punctCharArr2);
+console.log(punctCharArr3);
+console.log(punctCharArr4);
+console.log(punctCharArr5);
+
