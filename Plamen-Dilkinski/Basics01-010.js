520597d39a7f52873c3550518b52de303962382c
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-09 23:11:38 +0300

    solution of task Basics01-010

---
+++ Plamen-Dilkinski/Basics01-010.js
@@ -0,0 +1,20 @@
+/*      Declare ten variables. On the first five - assign the first five prime numbers.
+        On the second five numbers, do the same, but multiply each value by 3. Print all of them 
+*/
+
+//      5 variables where the first five primer numbers are assigned
+let varPrime1 = 1;
+let varPrime2 = 3;
+let varPrime3 = 5;
+let varPrime4 = 7;
+let varPrime5 = 9;
+
+// variables with first fiew prime numbers multipled by 3
+
+let varMultiply = 3;
+let varMultiply2 = 9;
+let varMultiply3 = 15;
+let varMultiply4 = 21;
+let varMultiply5 = 27;
+
+console.log("Printing all variables: "+ +varPrime1+", "+varPrime2+", "+varPrime3+", "+varPrime4+", "+varPrime5+", " +varMultiply+", "+varMultiply2+", " +varMultiply3+", "+varMultiply4+", "+varMultiply5);
