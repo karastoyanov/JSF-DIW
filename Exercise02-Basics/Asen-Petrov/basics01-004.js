aa3fe5049647800c41b5e4a3f132ca2e166e84ad
Asen Petrov <106155007+asenpetrov@users.noreply.github.com>
2024-05-10 00:18:55 +0300

    deleted arrows and comments

--- Exercise02-Basics/Asen-Petrov/basics01-004.js
+++ Exercise02-Basics/Asen-Petrov/basics01-004.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
 let alphChar1 = 'a';
 let alphChar2 = 'b';
 let alphChar3 = 'c';
 let alphChar4 = 'd';
 let alphChar5 = 'f';
-=======
-
-// try to solve it without array and without for loop
-// use only the things from Lesson 03
-
-/*
-let chars = ['a', 'b', 'c', 'd', 'f'];
-for (let index = 0; index < chars.length; index++) {
-    console.log(chars[index])
-    
-}
->>>>>>> 545c8baa4218fd3e98d59fae21ff9b6024046712
 
 console.log(alphChar1);
 console.log(alphChar2);
@@ -47,12 +34,3 @@ console.log(punctCharArr3);
 console.log(punctCharArr4);
 console.log(punctCharArr5);
 
-<<<<<<< HEAD
-=======
-let punctCharArr = ["/", "?", "!", ".", ","];
-for (let index = 0; index < chars.length; index++) {
-    console.log(punctCharArr[index])
-    
-}
-*/
->>>>>>> 545c8baa4218fd3e98d59fae21ff9b6024046712
