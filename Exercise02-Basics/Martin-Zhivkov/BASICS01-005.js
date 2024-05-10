7435e9757d006e87eba7892d921445627eed9749
martinzhivkov1 <167533793+martinzhivkov1@users.noreply.github.com>
2024-05-10 10:30:41 +0300

    fixing the whitespaces in BASICS01-005.js

--- Exercise02-Basics/Martin-Zhivkov/BASICS01-005.js
+++ Exercise02-Basics/Martin-Zhivkov/BASICS01-005.js
@@ -11,8 +11,8 @@ let year = 24;
 let date = 5.1;
 let duedate = 1.3;
 
-console.log (day);
-console.log (month);
-console.log (year);
-console.log (date);
-console.log (duedate);
+console.log(day);
+console.log(month);
+console.log(year);
+console.log(date);
+console.log(duedate);
