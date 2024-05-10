9de7d27d67e5f3b2283abcb1b1caf54c4a954091
Svilen Ivanov <svilen.ivanov@doitwise.com>
2024-05-10 09:28:10 +0300

    More excercises

---
+++ Exercise02-Basics/Basics 03 – Operations, Operators, Precedence/BASICS03-019.js
@@ -0,0 +1,11 @@
+/*
+BASICS03-019: Write a JavaScript program to convert km/h into m/s. Print on the console an
+appropriate message.
+*/
+
+let kmPerHour = 40;
+
+let mPerSecond = kmPerHour * 1000 / 3600;
+
+console.log (kmPerHour, "km/h is", mPerSecond, "m/s")
+
