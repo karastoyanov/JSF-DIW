e8b8d96496c058c77fcc14d507e3dedddccc2ef5
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-10 12:10:53 +0300

    New branch

---
+++ Exercise01-Git/Ivan-Ivanov/BASICS01/BASICS01-001.js
@@ -0,0 +1,12 @@
+//BASICS01-001: Write ten correct identifiers, following the camelCase naming convention.
+
+let firstName = "Ivan";
+let lastName = "Ivanov";
+let currentPosition = "TC";
+let currentAddress = "Sofia";
+let mobileNumber = "0896323232";
+let country = "Bulgaria";
+let cityName = "Sofia";
+let district = "Mladost";
+let floorNumber = "32";
+let officeNumber = "4.3";
