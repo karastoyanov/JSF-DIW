7f3729adf160e07287edb4a5caf574e4824a77b6
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:22:01 +0300

    006

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-006.js
@@ -0,0 +1,14 @@
+//BASICS01-006: Declare ten variables, assign the numbers from 1 to 10 and print the even numbers on the console.
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+let g = 7;
+let h = 8;
+let i = 9;
+let j = 10;
+
+console.log(b, d, f, h, j);
