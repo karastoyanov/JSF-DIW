df0996d2ff991b744a0c372064717490d19952a2
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:37:39 +0300

    01

--- Exercise02-Basics/Ivan-Ivanov/BASICS02/BASICS02-001.js
+++ Exercise02-Basics/Ivan-Ivanov/BASICS02/BASICS02-001.js
@@ -1,2 +1,9 @@
 //BASICS02-001: Declare five variables. Assign them with the ASCII codes of the first five English capital alphabet characters. Print them on the console.
 
+let a = String.fromCharCode(65);
+let b = String.fromCharCode(66);
+let c = String.fromCharCode(67);
+let d = String.fromCharCode(68);
+let e = String.fromCharCode(69);
+
+console.log(a, b, c, d, e);
