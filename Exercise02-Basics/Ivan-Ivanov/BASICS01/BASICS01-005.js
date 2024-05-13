91c33557c3f69b67221d150bb9bbefab8215bbfc
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:20:41 +0300

    005

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-005.js
@@ -0,0 +1,9 @@
+//Declare five variables, assign some numbers, and print them on the console.
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(`${a}, ${b}, ${c} ,${d}, ${e}`);
