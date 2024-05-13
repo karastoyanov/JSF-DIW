0014d6f9205559e24aec60609f338149df642b3d
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:18:48 +0300

    003

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-003.js
@@ -0,0 +1,15 @@
+/*BASICS01-003: Imagine, you are working for a cloud provider and are responsible for the
+servers. You must write a program to list and describe the servers. Declare ten
+variables, which you might need in your program.
+*/
+
+let hostname = "Test";
+let osPlatform = "Linux";
+let memory = "128tb"
+let cpuCount = 64;
+let country = "Bulgaria";
+let hostingCity = "Sofia";
+let virtualizationPlatform = "VMWare";
+let osDomainName = "OS.domain";
+let dnsDomainName = "DNS.domain";
+let ipAddress = "127.0.0.1"
