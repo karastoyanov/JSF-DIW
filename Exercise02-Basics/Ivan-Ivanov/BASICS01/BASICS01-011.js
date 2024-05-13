418678334b9718e0f52f5e69ab6e3642ede08ea9
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:27:23 +0300

    011

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-011.js
@@ -0,0 +1,14 @@
+//BASICS01-011: Declare ten variables. Assign them the first ten prime numbers. Print the numbers in reverse order.
+
+let a = 2;
+let b = 3;
+let c = 5;
+let d = 7;
+let e = 11;
+let f = 13;
+let g = 17;
+let h = 19;
+let i = 23;
+let j = 29;
+
+console.log(j, i, h, g, f, e, d, c, b, a);
