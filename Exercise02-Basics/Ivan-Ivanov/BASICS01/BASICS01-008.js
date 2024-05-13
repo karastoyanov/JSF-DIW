b4c4573d2cc4d3cc0c4c0533942f99a3ba6af334
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:23:59 +0300

    008

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-008.js
@@ -0,0 +1,14 @@
+//BASICS01-008: Declare ten variables, assign the numbers from 100 to 109 and print the last two odd numbers on the console.
+
+let a = 100;
+let b = 101;
+let c = 102;
+let d = 103;
+let e = 104;
+let f = 105;
+let g = 106;
+let h = 107;
+let i = 108;
+let j = 109;
+
+    console.log(h, j);
