c5ee07497eb6e8d986dee9c35e0e0d8dce55f8ed
Nenad Gvozdenovic <nenad.gvozdenovic@doitwise.com>
2024-05-13 09:34:27 +0300

    adding basics I and II

---
+++ Exercise02-Basics/Nenad-Gvozdenovic/basics01-004.js
@@ -0,0 +1,40 @@
+/*
+BASICS01-004: Print on the console five alphabet characters, five numbers, five punctuation 
+characters. 
+*/
+
+let alphaChar1 = "L";
+let alphaChar2 = "M";
+let alphaChar3 = "F";
+let alphaChar4 = "A";
+let alphaChar5 = "O";
+
+let numChar1 = 1;
+let numChar2 = 10;
+let numChar3 = 100;
+let numChar4 = 1000;
+let numChar5 = 10000;
+
+let puncSign1 = "!"
+let puncSign2 = "_"
+let puncSign3 = "."
+let puncSign4 = ","
+let puncSign5 = "-"
+
+console.log(alphaChar1);
+console.log(alphaChar2);
+console.log(alphaChar3);
+console.log(alphaChar4);
+console.log(alphaChar5);
+
+console.log(numChar1);
+console.log(numChar2);
+console.log(numChar3);
+console.log(numChar4);
+console.log(numChar5);
+
+console.log(puncSign1);
+console.log(puncSign2);
+console.log(puncSign3);
+console.log(puncSign4);
+console.log(puncSign5);
