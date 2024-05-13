7958e49b3ed36fb1e3d1f95a4ba6e3f90319912f
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:22:57 +0300

    007

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-007.js
@@ -0,0 +1,14 @@
+//BASICS01-007: Declare ten variables, assign the numbers from 1 to 10 and print the first three odd numbers on the console.
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+let g = 7;
+let h = 8;
+let i = 9;
+let j = 10;
+
+console.log(a, c, e);    // 1, 3, 5
