f8e11cb6c3f43b673a0cd9def0b83fc5175de4e3
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:18:03 +0300

    002

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-002.js
@@ -0,0 +1,14 @@
+/*BASICS01-002: Imagine, you are solving a math problem. Declare ten variables, which you
+might need in your program.
+*/
+
+let side = 55;
+let a = 10;
+let b = 15;
+let x = 12;
+let y = 12;
+let discount = 0.5;
+let mtg = 0.1;
+let height = 20;
+let r = 10;
+let dist = 50;
