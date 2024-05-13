fa0c348c54309031ddb8bdb0021bffa6154eac16
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:26:01 +0300

    010

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-010.js
@@ -0,0 +1,17 @@
+/*BASICS01-010: Declare ten variables. On the first five - assign the first five prime numbers.
+On the second five numbers, do the same, but multiply each value by 3. Print all of them
+on the console.
+*/
+
+let a = 2;
+let b = 3;
+let c = 5;
+let d = 7;
+let e = 11;
+let f = a*3;
+let g = b*3;
+let h = c*3;
+let i = d*3;
+let j = e*3;
+
+console.log(a, b, c, d, e, f, g, h, i, j);
