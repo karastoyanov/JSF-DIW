dfd1af78f4e317eeca526dadf7346f3ed9ed15ce
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:48:06 +0300

    02

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS02/BASICS02-002.js
@@ -0,0 +1,9 @@
+//BASICS02-002: Declare five variables. Assign them with the ASCII codes of the last five English lowercase alphabet characters. Print them on the console.
+
+let a = String.fromCharCode(118);
+let b = String.fromCharCode(119);
+let c = String.fromCharCode(120);
+let d = String.fromCharCode(121);
+let e = String.fromCharCode(122);
+
+console.log(a, b, c, d, e);
