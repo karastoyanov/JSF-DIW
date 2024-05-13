7de57b429e524b5bd7849485da090c432d07e209
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:24:49 +0300

    009

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-009.js
@@ -0,0 +1,9 @@
+//BASICS01-009: Declare five variables, assign the first five prime numbers, and print them on the console.
+
+let a = 2;
+let b = 3;
+let c = 5;
+let d = 7;
+let e = 11;
+
+console.log(a, b, c, d, e);
