851c87c298747062ea8567da942a5a33019c49b6
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-13 14:20:03 +0300

    004

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS01/BASICS01-004.js
@@ -0,0 +1,5 @@
+//BASICS01-004: Print on the console five alphabet characters, five numbers, five punctuation characters.
+
+console.log("A", "B", "C", "D", "E");
+console.log(1, 2, 3, 4, 5);
+console.log(",", ".", ":", "'", ";");
