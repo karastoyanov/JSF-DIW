e7a5684782094cab9568d689810417a367bcb398
Asen Petrov <106155007+asenpetrov@users.noreply.github.com>
2024-05-14 20:37:03 +0300

    basics02-010

---
+++ Exercise02-Basics/Asen-Petrov/basics02-010.js
@@ -0,0 +1,12 @@
+let uniCode1 = '🔞'.codePointAt(0);
+let uniCode2 = '🎩'.codePointAt(0);
+let uniCode3 = '😡'.codePointAt(0);
+let uniCode4 = '🏋'.codePointAt(0);
+let uniCode5 = '🚗'.codePointAt(0);
+
+console.log(`${uniCode1.toString(2)}, ${uniCode1.toString(8)}, ${uniCode1.toString(10)}, ${uniCode1.toString(16)}`);
+console.log(`${uniCode2.toString(2)}, ${uniCode2.toString(8)}, ${uniCode2.toString(10)}, ${uniCode2.toString(16)}`);
+console.log(`${uniCode3.toString(2)}, ${uniCode3.toString(8)}, ${uniCode3.toString(10)}, ${uniCode3.toString(16)}`);
+console.log(`${uniCode4.toString(2)}, ${uniCode4.toString(8)}, ${uniCode4.toString(10)}, ${uniCode4.toString(16)}`);
+console.log(`${uniCode5.toString(2)}, ${uniCode5.toString(8)}, ${uniCode5.toString(10)}, ${uniCode5.toString(16)}`);
+
