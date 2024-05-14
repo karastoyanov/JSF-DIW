31b54b5815a26ace58f8a1436efe07d19ada767b
Asen Petrov <106155007+asenpetrov@users.noreply.github.com>
2024-05-14 21:02:05 +0300

    basics02-014

--- Exercise02-Basics/Asen-Petrov/basics02-013.js
+++ Exercise02-Basics/Asen-Petrov/basics02-013.js
@@ -8,3 +8,6 @@ let number2 = '1';
 
 let result2 = letter1 > number2;
 console.log(result2);
+
+//Comparing char 'A' to num 1, the result is false
+//Comparing char 'A' to char '1', the result is true (1 before A in ASCII table)
---
+++ Exercise02-Basics/Asen-Petrov/basics02-014.js
@@ -0,0 +1,12 @@
+let char = 'A';
+
+if (char != char.toUpperCase()) {
+    let capitalChar = char.toUpperCase();
+    console.log(capitalChar);
+}
+else{
+    let lowerChar = char.toLowerCase();
+    console.log(lowerChar);
+}
+
+//Did not understand the suggested hint/way of solving the problem
