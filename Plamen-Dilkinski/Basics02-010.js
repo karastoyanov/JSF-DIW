9f0b08f9c87a973501146fa0d5400c3b03ace160
ivaylochenchev <58465111+ivaylochenchev@users.noreply.github.com>
2024-05-16 01:12:12 +0300

    Update Basics02-010.js
    For each of the variables ... on one line

--- Plamen-Dilkinski/Basics02-010.js
+++ Plamen-Dilkinski/Basics02-010.js
@@ -6,4 +6,14 @@ let rand3 = "🤪".codePointAt(0);
 let rand4 = "👍".codePointAt(0);
 let rand5 = "🕺".codePointAt(0);
 
-console.log(rand1.toString(2)+", "+(rand1.toString(8))+", "+(rand1.toString(10)+", "+rand1.toString(16)+", "+rand2.toString(2)+", "+rand2.toString(8))+", "+rand2.toString(10)+", "+rand2.toString(16)+", "+rand3.toString(2)+", "+rand3.toString(8)+", "+rand3.toString(10)+", "+rand3.toString(16)+", "+rand4.toString(2)+", "+rand4.toString(8)+", "+rand4.toString(10)+", "+rand4.toString(16)+", "+rand5.toString(2)+", "+rand5.toString(8)+", "+rand5.toString(10)+", "+rand5.toString(16));
+// console.log(rand1.toString(2)+", "+(rand1.toString(8))+", "+(rand1.toString(10)+", "+rand1.toString(16)+", "+rand2.toString(2)+", "+rand2.toString(8))+", "+rand2.toString(10)+", "+rand2.toString(16)+", "+rand3.toString(2)+", "+rand3.toString(8)+", "+rand3.toString(10)+", "+rand3.toString(16)+", "+rand4.toString(2)+", "+rand4.toString(8)+", "+rand4.toString(10)+", "+rand4.toString(16)+", "+rand5.toString(2)+", "+rand5.toString(8)+", "+rand5.toString(10)+", "+rand5.toString(16));
+
+// !! it is not by condition -> on one line
+
+// console.log(randomEmoji.toString(2)+", "+(randomEmoji.toString(8))+", "+(randomEmoji.toString(10)+", "+randomEmoji.toString(16) + '\n' + randomEmoji2.toString(2)+", "+randomEmoji2.toString(8))+", "+randomEmoji2.toString(10)+", "+randomEmoji2.toString(16)+ '\n'+ randomEmoji3.toString(2)+ '\n' +randomEmoji3.toString(8)+", "+randomEmoji3.toString(10)+", "+randomEmoji3.toString(16)+ '\n' +randomEmoji4.toString(2)+", "+randomEmoji4.toString(8)+", "+randomEmoji4.toString(10)+", "+randomEmoji4.toString(16)+ '\n' + randomEmoji5.toString(2)+", "+randomEmoji5.toString(8)+", "+randomEmoji5.toString(10)+", "+randomEmoji5.toString(16));
+// or
+console.log(rand1.toString(2)+", "+rand1.toString(8)+", "+rand1.toString(10)+", "+rand1.toString(16));
+console.log(rand2.toString(2)+", "+rand2.toString(8)+", "+rand2.toString(10)+", "+rand2.toString(16));
+console.log(rand3.toString(2)+", "+rand3.toString(8)+", "+rand3.toString(10)+", "+rand3.toString(16)); 
+console.log(rand4.toString(2)+", "+rand4.toString(8)+", "+rand4.toString(10)+", "+rand4.toString(16));
+console.log(rand5.toString(2)+", "+rand5.toString(8)+", "+rand5.toString(10)+", "+rand5.toString(16));
