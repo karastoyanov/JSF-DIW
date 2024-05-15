098f04e5d6f7c94b1e1829335f212129e6906076
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-15 23:18:58 +0300

    BASICS02-011 Solution

---
+++ Alex-Pilarski/BASICS02-011.js
@@ -0,0 +1,7 @@
+// Declare two variables. Assign them with two English capital alphabet characters. 
+// Compare them with the “lower than” operator (<) and print on the console the result.
+
+let letter = "A";
+let letter2 = "D";
+
+console.log(letter<letter2);
