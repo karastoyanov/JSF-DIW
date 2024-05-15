ad69eb8c7f614d1c1bd3b766c93c6f5b5b8991dd
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-15 23:25:26 +0300

    BASICS02-012 Solution

---
+++ Alex-Pilarski/BASICS02-012.js
@@ -0,0 +1,11 @@
+// Declare two variables. Assign them with two English alphabet characters –
+// one in capital and the other one in lowercase. Compare them with the “lower than”
+// operator (<) and print on the console the result. Can you describe the result?
+
+let upperCase = "D";
+let lowerCase = "e";
+
+console.log(upperCase<lowerCase);
+
+// The Unicode value of the character 'D' is less than the Unicode value of the character 'e'. 
+// This is because the Unicode value of 'D' is numerically lower than the Unicode value of 'e'.
