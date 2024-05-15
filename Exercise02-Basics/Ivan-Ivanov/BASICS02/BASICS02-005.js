f5ed460287c8083cedd4df4a3f180ff294313af8
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-15 16:07:40 +0300

    05

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS02/BASICS02-005.js
@@ -0,0 +1,12 @@
+/*BASICS02-005: Declare five variables. Assign them with the UNICODE codes of randomly
+chosen emoji characters. Print them on the console – on one line, separated with four
+spaces.
+*/
+
+let a = '\u{1F600}';
+let b = '\u{1F643}';
+let c = '\u{1F929}';
+let d = '\u{1F61B}';
+let e = '\u{1F60E}';
+
+console.log(a, b, c, d, e);
