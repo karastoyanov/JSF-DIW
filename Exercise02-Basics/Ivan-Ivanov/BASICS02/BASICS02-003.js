80d97a0ac49da3570399700646ea0913f269d59f
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-15 15:40:41 +0300

    03

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS02/BASICS02-003.js
@@ -0,0 +1,9 @@
+//BASICS02-003: Declare five variables. Assign them with the ASCII codes of randomly chosen punctuation characters. Print them on the console.
+
+let a = String.fromCharCode(33);
+let b = String.fromCharCode(34);
+let c = String.fromCharCode(39);
+let d = String.fromCharCode(45);
+let e = String.fromCharCode(46);
+
+console.log(a, b, c, d, e);
