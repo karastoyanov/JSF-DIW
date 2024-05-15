32e6aeb0dfb1ac862804ee0df7ff27b48dd90cb8
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-16 01:11:02 +0300

    New Changes made

--- Alex-Pilarski/BASICS02-010.js
+++ Alex-Pilarski/BASICS02-010.js
@@ -8,12 +8,6 @@ let randomEmoji3 = "😆".codePointAt(0);
 let randomEmoji4 = "😄".codePointAt(0);
 let randomEmoji5 = "😁".codePointAt(0);
 
-// console.log(randomEmoji.toString(2)+", "+(randomEmoji.toString(8))+", "+(randomEmoji.toString(10)+", "+randomEmoji.toString(16)+", "+randomEmoji2.toString(2)+", "+randomEmoji2.toString(8))+", "+randomEmoji2.toString(10)+", "+randomEmoji2.toString(16)+", "+randomEmoji3.toString(2)+", "+randomEmoji3.toString(8)+", "+randomEmoji3.toString(10)+", "+randomEmoji3.toString(16)+", "+randomEmoji4.toString(2)+", "+randomEmoji4.toString(8)+", "+randomEmoji4.toString(10)+", "+randomEmoji4.toString(16)+", "+randomEmoji5.toString(2)+", "+randomEmoji5.toString(8)+", "+randomEmoji5.toString(10)+", "+randomEmoji5.toString(16));
-
-// !! it is not by condition -> on one line
-
-// console.log(randomEmoji.toString(2)+", "+(randomEmoji.toString(8))+", "+(randomEmoji.toString(10)+", "+randomEmoji.toString(16) + '\n' + randomEmoji2.toString(2)+", "+randomEmoji2.toString(8))+", "+randomEmoji2.toString(10)+", "+randomEmoji2.toString(16)+ '\n'+ randomEmoji3.toString(2)+ '\n' +randomEmoji3.toString(8)+", "+randomEmoji3.toString(10)+", "+randomEmoji3.toString(16)+ '\n' +randomEmoji4.toString(2)+", "+randomEmoji4.toString(8)+", "+randomEmoji4.toString(10)+", "+randomEmoji4.toString(16)+ '\n' + randomEmoji5.toString(2)+", "+randomEmoji5.toString(8)+", "+randomEmoji5.toString(10)+", "+randomEmoji5.toString(16));
-// or
 console.log(randomEmoji.toString(2)+", "+randomEmoji.toString(8)+", "+randomEmoji.toString(10)+", "+randomEmoji.toString(16));
 console.log(randomEmoji2.toString(2)+", "+randomEmoji2.toString(8)+", "+randomEmoji2.toString(10)+", "+randomEmoji2.toString(16));
 console.log(randomEmoji3.toString(2)+", "+randomEmoji3.toString(8)+", "+randomEmoji3.toString(10)+", "+randomEmoji3.toString(16)); 
