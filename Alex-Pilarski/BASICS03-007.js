05cda59a7d70a413d75ab1816b36c47e236d0f6d
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-16 01:26:15 +0300

    BASICS03-007 Solution

--- Alex-Pilarski/BASICS03-007.js
+++ Alex-Pilarski/BASICS03-007.js
@@ -1,13 +1,9 @@
 // Declare a constant and assign one digit. Print on one line the constant, the
 // power of two (N2), the power of three (N3) on the console.
 
-
-// Declare a constant and assign one digit
 const num = 5;
 
-// Calculate the power of two and three
 const powerOfTwo = Math.pow(num, 2);
 const powerOfThree = Math.pow(num, 3);
 
-// Print the constant, the power of two, and the power of three on one line
 console.log(`Constant: ${num}, Power of Two: ${powerOfTwo}, Power of Three: ${powerOfThree}`);
