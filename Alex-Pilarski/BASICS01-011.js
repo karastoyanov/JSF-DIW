d184c37180304921ef4fdba7d66c147eb54c5162
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-15 22:15:56 +0300

    BASICS01-0101 Solution

---
+++ Alex-Pilarski/BASICS01-011.js
@@ -0,0 +1,14 @@
+// Declare ten variables. Assign them the first ten prime numbers. Print the numbers in reverse order.
+
+let n1 = 2;
+let n2 = 3;
+let n3 = 5;
+let n4 = 7;
+let n5 = 11;
+let n6 = 13;
+let n7 = 17;
+let n8 = 19;
+let n9 = 23;
+let n10 = 29;
+
+console.log("All Prime numbers in reverse order: " + n10 + ", " + n9 + ", " + n8 + ", " + n7 + ", " + n6 + ", " + n5 + ", " + n4 + ", " + n3 + ", " + n2 + ", " + n1);
