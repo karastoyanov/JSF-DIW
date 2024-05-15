d7428e02fc0fddaf1264ee520f789f5186842737
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-16 00:58:34 +0300

    BASICS03-006 Solution

---
+++ Alex-Pilarski/BASICS03-006.js
@@ -0,0 +1,8 @@
+// Declare one variable, assign integer number. Check if the variable contains
+// an even number. Print on the console appropriate message.
+
+let number = 15;
+
+let evenOrOdd = number % 2 === 0 ? "The number is even." : "The number is odd.";
+
+console.log(evenOrOdd);
