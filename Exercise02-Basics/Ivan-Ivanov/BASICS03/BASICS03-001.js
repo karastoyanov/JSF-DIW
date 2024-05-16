3c8f470593def281a0417c12a8648e0000a80fdb
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-16 16:05:46 +0300

    03-001

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS03/BASICS03-001.js
@@ -0,0 +1,8 @@
+//BASICS03-001: Declare two variables and assign them two integer numbers. Print on the console the result of their division.
+
+let a = 20;
+let b = 10;
+
+let res = a/b;
+
+console.log(res)
