b949d57ac2798f568b97116ce75c7a21a47746a2
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-05-16 11:27:33 +0300

    BASICS03-021 Solution

--- Alex-Pilarski/BASICS03-021.js
+++ Alex-Pilarski/BASICS03-021.js
@@ -11,8 +11,10 @@ let r6 = a*--b;
 let r7 = a**++b*b;
 let r8 = a**++b//b;
 
-/* let r2 = a+++b;: This expression is incorrectly defined. ++ operator is incorrect
+/*
+let r2 = a+++b;: This expression is incorrectly defined. ++ operator is incorrect
 
 let r3 = a---b;: This expression is incorrectly defined. -- operator is incorrect
 
-let r8 = a**++b//b;: This expression is incorrectly defined becase of misplaced comment symbol (//).*/
+let r8 = a**++b//b;: This expression is incorrectly defined becase of misplaced comment symbol (//).
+*/
