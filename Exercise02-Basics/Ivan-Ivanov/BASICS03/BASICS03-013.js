308fb4e5aa9815aa489629dfe9419a25359207f0
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-16 16:48:52 +0300

    03-0014

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS03/BASICS03-013.js
@@ -0,0 +1,7 @@
+//BASICS03-013: Calculate and print on the console the perimeter (the length) of a circle.
+
+let d = 10;
+
+let p = Math.PI*d;
+
+console.log("Периметърът на окръжността е " + p.toFixed(2) + " см2")
---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS03/BASICS03-014.js
@@ -0,0 +1,7 @@
+//BASICS03-014: Calculate and print on the console the area of a circle.
+
+let r = 10;
+
+let s = Math.PI*r+r;
+
+console.log("Лицето на окръжността е " + s.toFixed(2) + " см2")
