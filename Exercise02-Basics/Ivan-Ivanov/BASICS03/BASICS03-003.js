e08984facc8b7994971e8621b8bb8fbbb8e28f51
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-16 16:11:50 +0300

    03-003

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS03/BASICS03-003.js
@@ -0,0 +1,13 @@
+/*
+BASICS03-003: Declare four variables. On two of them assign integer numbers. The third set
+with the division reminder. The fourth one set with the quotient (часiноiо -> mяoаiа
+часi оi sеoенqеiо). Print on the console the four variables with appropriate
+description.
+*/
+
+let a = 23;
+let b = 10;
+let c = a%b
+let d = Math.floor(a/b)
+
+console.log("a="+ a + " b=" + b + " The remainder is " + c + " The quotient is " + d)
