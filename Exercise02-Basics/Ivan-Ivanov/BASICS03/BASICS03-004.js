c643f38f25ee625f1409587b64a148ed872f3e54
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-16 16:14:43 +0300

    03-004

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS03/BASICS03-004.js
@@ -0,0 +1,9 @@
+/*
+BASICS03-004: Define a constant. Check and print on the console if the constant is positive,
+negative or zero. Hint: Use ternary operators. How many operators do you need?
+*/
+const a = -20;  
+
+const check = a > 0 ? 'positive' : (a < 0 ? 'negative' : 'zero');
+
+console.log(check);
