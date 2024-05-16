507116b1520c229264c44dd75be06f9030ad0b76
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-05-16 15:19:32 +0300

    12

---
+++ Exercise02-Basics/Ivan-Ivanov/BASICS02/BASICS02-012.js
@@ -0,0 +1,13 @@
+/*
+BASICS02-012: Declare two variables. Assign them with two English alphabet characters –
+one in capital and the other one in lowercase. Compare them with the “lower than”
+operator (<) and print on the console the result. Can you describe the result?
+*/
+
+
+let a = "A";
+let b = "a";
+
+let compare = a>b;
+
+console.log (compare)
