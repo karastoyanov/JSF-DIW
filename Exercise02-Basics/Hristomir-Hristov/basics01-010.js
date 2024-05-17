3c46eb7aaf727d0ad39d29ec0c004222efe14b19
hrhristoff <hristomir.hristoff@gmail.com>
2024-05-17 10:16:54 +0300

    Hristomir newest change

---
+++ Exercise02-Basics/Hristomir-Hristov/basics01-010.js
@@ -0,0 +1,6 @@
+/* 
+Declare ten variables. On the first five - assign the first five prime numbers.
+On the second five numbers, do the same, but multiply each value by 3. Print all of them
+on the console.
+*/
+
