820e517847fc73f549a02e908b0466d5dfad3d96
pdilkinski <plamen.dilkinski@gmail.com>
2024-05-22 17:36:32 +0300

    first task dtypes

---
+++ Plamen-Dilkinski/DTYPES01-001.js
@@ -0,0 +1,4 @@
+//      DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use different quotes)
+
+fistQuotes = 'Plamen Dilkinski';
+secondQuotes = "Plamen Dilkinski";
