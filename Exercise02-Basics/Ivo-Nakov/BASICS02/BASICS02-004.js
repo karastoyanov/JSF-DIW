be97eb1333798c438da0fb887a02767a6a39c881
Ivo Nakov <ivo.nakov@doitwise.com>
2024-05-22 14:14:06 +0300

    BASICS02-004--006

---
+++ Exercise02-Basics/Ivo-Nakov/BASICS02/BASICS02-004.js
@@ -0,0 +1,12 @@
+// BASICS02-004
+let emojiSmile = '\u263A';
+let emojiHeart = '\u2764';
+let emojiStar = '\u2B50';
+let emojiThumbsUp = '\u1F44D';
+let emojiSun = '\u2600';
+
+console.log(emojiSmile);
+console.log(emojiHeart);
+console.log(emojiStar);
+console.log(emojiThumbsUp);
+console.log(emojiSun);
---
+++ Exercise02-Basics/Ivo-Nakov/BASICS02/BASICS02-005.js
@@ -0,0 +1,8 @@
+// BASICS02-005
+let emojiSmile = '\u263A';
+let emojiHeart = '\u2764';
+let emojiStar = '\u2B50';
+let emojiThumbsUp = '\u1F44D';
+let emojiSun = '\u2600';
+
+console.log(emojiSmile + '    ' + emojiHeart + '    ' + emojiStar + '    ' + emojiThumbsUp + '    ' + emojiSun);
