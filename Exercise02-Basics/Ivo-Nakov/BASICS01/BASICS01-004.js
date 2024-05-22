9add4f2d9e821b21dc30813df703181746e1a830
Ivo Nakov <ivo.nakov@doitwise.com>
2024-05-22 13:55:08 +0300

    BASICS01-004--006

---
+++ Exercise02-Basics/Ivo-Nakov/BASICS01/BASICS01-004.js
@@ -0,0 +1,24 @@
+// BASICS01-004
+let fiveAlphabet = 'QWERT';
+let fiveNumbers = '12345';
+let fivePunctuation = '!@#$%';
+
+console.log('fiveAlphabet');
+console.log('fiveNumbers');
+console.log('fivePunctuation');
+// OR
+console.log('A');
+console.log('B');
+console.log('C');
+console.log('D');
+console.log('E');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log('!');
+console.log('@');
+console.log('#');
+console.log('$');
+console.log('%');
---
+++ Exercise02-Basics/Ivo-Nakov/BASICS01/BASICS01-006.js
@@ -0,0 +1,17 @@
+// BASICS01-006
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+let num10 = 10;
+
+console.log(num2);
+console.log(num4);
+console.log(num6);
+console.log(num8);
+console.log(num10);
