313c21cfd74a5ffbe8eb7e17361405d0fed047ff
Ivo Nakov <ivo.nakov@doitwise.com>
2024-05-22 14:02:40 +0300

    BASICS01-007--009

---
+++ Exercise02-Basics/Ivo-Nakov/BASICS01/BASICS01-009.js
@@ -0,0 +1,12 @@
+// BASICS01-009
+let prime1 = 2;
+let prime2 = 3;
+let prime3 = 5;
+let prime4 = 7;
+let prime5 = 11;
+
+console.log(prime1);
+console.log(prime2);
+console.log(prime3);
+console.log(prime4);
+console.log(prime5);
