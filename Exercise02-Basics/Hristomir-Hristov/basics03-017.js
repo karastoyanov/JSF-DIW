9c76ddb585c00d5ae212d9f777384dc82cda9781
hrhristoff <hristomir.hristoff@gmail.com>
2024-05-23 17:55:16 +0300

    bacics 3 16-22

---
+++ Exercise02-Basics/Hristomir-Hristov/basics03-017.js
@@ -0,0 +1,10 @@
+/*
+
+Write a JavaScript program to convert degrees in radians. Print on the console
+an appropriate message.
+
+*/
+
+let degrees = 90;
+let radians = degrees * (Math.PI / 180);
+console.log("A " +degrees + " degree angle equals to " +radians + " radians.");
