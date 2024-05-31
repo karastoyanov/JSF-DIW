8e55278fc174622c27666732bbcf31d85eb6dbe6
Svilen Ivanov <svilen.ivanov@doitwise.com>
2024-05-31 16:08:58 +0300

    and again

--- Additional-Exercises/ADDITIONAL01-013.js
+++ Additional-Exercises/ADDITIONAL01-013.js
@@ -3,6 +3,6 @@ Define a variable with a sentence.
 Replace all spaces with hyphens and print the modified string on the console.
 */
 
-var sentence = "Everybody likes perniishka martenitsa!";
+let sentence = "Everybody likes perniishka martenitsa!";
 
 console.log(sentence.replace(/ /g, '-'));
