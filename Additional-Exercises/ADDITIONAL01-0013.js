f5abe1f85f8a1032f125c6748da5f4cfbcaab80b
Svilen Ivanov <svilen.ivanov@doitwise.com>
2024-05-31 16:05:16 +0300

    ADDITIONAL01-013

---
+++ Additional-Exercises/ADDITIONAL01-0013.js
@@ -0,0 +1,8 @@
+/*ADDITIONAL01-013
+Define a variable with a sentence.
+Replace all spaces with hyphens and print the modified string on the console.
+*/
+
+var sentence = "Everybody likes perniishka martenitsa!";
+
+console.log(sentence.replace(/ /g, '-'));
