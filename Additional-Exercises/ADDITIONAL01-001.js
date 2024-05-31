27cfd547bf7ee7cccf44261029262d90ad9580a1
Iva Georgieva <iva.georgieva@doitwise.com>
2024-05-31 10:52:22 +0300

    Additional01-001

---
+++ Additional-Exercises/ADDITIONAL01-001.js
@@ -0,0 +1,25 @@
+/*  ADDITIONAL01-001:
+    Define ten string variables and assign them your favorite book titles. Print each variable on the console on a new line. */
+
+
+    let title1 = 'Harry Potter';
+    let title2 = 'To Kill a Mockingbird';
+    let title3 = '1984';
+    let title4 = 'Pride and Prejudice';
+    let title5 = 'Dune';
+    let title6 = 'Moby-Dick';
+    let title7 = 'War and Peace';
+    let title8 = 'The Catcher in the Rye';
+    let title9 = 'Brave New World';
+    let title10 = 'The Hobbit';
+
+    console.log(title1);
+    console.log(title2);
+    console.log(title3);
+    console.log(title4);
+    console.log(title5);
+    console.log(title6);
+    console.log(title7);
+    console.log(title8);
+    console.log(title9);
+    console.log(title10);
---
+++ Iva-Georgieva/04.js
@@ -0,0 +1,6 @@
+// DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use
+// diWerent quotes).
+
+let nameFirstLast = "Iva Georgieva";
+let nmFL = 'Iva Georgieva';
+
