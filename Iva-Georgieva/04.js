3de9b18380f67ce31c0bcfeec7cedd3a20aee2bf
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-05-31 13:33:26 +0300

    dir structure fixed

--- Iva-Georgieva/04.js
+++
@@ -1,6 +0,0 @@
-// DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use
-// diWerent quotes).
-
-let nameFirstLast = "Iva Georgieva";
-let nmFL = 'Iva Georgieva';
-
