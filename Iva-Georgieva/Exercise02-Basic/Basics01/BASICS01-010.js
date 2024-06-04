7694eefa09b6a4fc4acc9032cb4e88028b1b5c30
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2024-06-04 09:29:04 +0300

    Update BASICS01-010.js
    BASICS01-010 indentation fixed

--- Iva-Georgieva/Exercise02-Basic/Basics01/BASICS01-010.js
+++ Iva-Georgieva/Exercise02-Basic/Basics01/BASICS01-010.js
@@ -1,27 +1,27 @@
 /* BASICS01-010: Declare ten variables. On the first five - assign the first five prime numbers. 
      On the second five numbers, do the same, but multiply each value by 3. Print all of them on the console. */
 
-     let primeNumber1;
-     let primeNumber2;
-     let primeNumber3;
-     let primeNumber4;
-     let primeNumber5;
-     let primeNumber6;
-     let primeNumber7;
-     let primeNumber8;
-     let primeNumber9;
-     let primeNumber10;
+let primeNumber1;
+let primeNumber2;
+let primeNumber3;
+let primeNumber4;
+let primeNumber5;
+let primeNumber6;
+let primeNumber7;
+let primeNumber8;
+let primeNumber9;
+let primeNumber10;
 
-     primeNumber1 = 1;
-     primeNumber2 = 2;
-     primeNumber3 = 3;
-     primeNumber4 = 5;
-     primeNumber5 = 7;
-     primeNumber6 = primeNumber1 * 3;
-     primeNumber7 = primeNumber2 * 3;
-     primeNumber8 = primeNumber3 * 3;
-     primeNumber9 = primeNumber4 * 3;
-     primeNumber10 = primeNumber5 * 5;       
+primeNumber1 = 1;
+primeNumber2 = 2;
+primeNumber3 = 3;
+primeNumber4 = 5;
+primeNumber5 = 7;
+primeNumber6 = primeNumber1 * 3;
+primeNumber7 = primeNumber2 * 3;
+primeNumber8 = primeNumber3 * 3;
+primeNumber9 = primeNumber4 * 3;
+primeNumber10 = primeNumber5 * 5;       
 
-      console.log(primeNumber1, primeNumber2, primeNumber3, primeNumber4, primeNumber5, primeNumber6, primeNumber7, primeNumber8, primeNumber9, primeNumber10);
+console.log(primeNumber1, primeNumber2, primeNumber3, primeNumber4, primeNumber5, primeNumber6, primeNumber7, primeNumber8, primeNumber9, primeNumber10);
 
