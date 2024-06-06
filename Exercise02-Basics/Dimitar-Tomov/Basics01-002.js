33a9f39d9372b40b3dc4d519d480ca814ccf9250
DimitarTom <167531147+DimitarTom@users.noreply.github.com>
2024-06-06 14:52:01 +0200

    EXE01_002

---
+++ Exercise02-Basics/Dimitar-Tomov/Basics01-002.js
@@ -0,0 +1,15 @@
+/* Imagine, you are solving a math problem. Declare ten variables, which you 
+might need in your program*/
+
+let numberOne;
+let numberTwo;
+let operator;
+let result;
+let addition;
+let multiplication;
+let division;
+const PI_VARIABLE = 3.14;
+const VOLUME = 15;
+const SOUND_SPEED = 1073;
+console.log(SOUND_SPEED);
+console.log('SOUND_SPEED');
