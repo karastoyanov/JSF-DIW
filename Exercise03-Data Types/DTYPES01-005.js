b6859a518a1d30fd16b1ea5a03d5c759da3ddc3b
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-06-06 13:31:54 +0300

    DTYPES-005

---
+++ Exercise03-Data Types/DTYPES01-005.js
@@ -0,0 +1,9 @@
+/* Define a string variable with following format:
+My name’s <name>. I’m <age> years old.
+Previously define the name and the age values. They will not be changed during the
+further code. */
+
+let name = 'Alex'
+let age = 33;
+
+let result = `My name's ${name}. I'm ${age} years old.`
