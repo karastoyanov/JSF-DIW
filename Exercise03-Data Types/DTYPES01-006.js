7e19ca745f9a05a3510680a0c920224ac55eea9c
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-06-06 13:41:48 +0300

    DTYPES-006

---
+++ Exercise03-Data Types/DTYPES01-006.js
@@ -0,0 +1,10 @@
+/* Define four string variables with your three names (first, middle and family).
+The fourth variable initialize with all names, separated with a tab character. Print on the
+console the length of each variable. */
+
+let firstName = 'Alexander';
+let middleName = 'Plamenov';
+let lastName = 'Pilarski';
+let allNames = `${firstName}    ${middleName}   ${lastName}`
+
+console.log(allNames);
