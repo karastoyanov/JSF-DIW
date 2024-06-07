86358764050cdb6ea834a5b0c8274849fd6c3adf
hrhristoff <hristomir.hristoff@gmail.com>
2024-06-07 15:37:54 +0300

    datatypes2 1-8

---
+++ Exercise03-DataTypes/Hristomir-Hristov/DTYPES02-004.js
@@ -0,0 +1,8 @@
+/*
+ Write a program in JavaScript to check if a given number is even or odd. // use a ternary operator and string template
+ */
+
+let n = 56;
+let isEven = n % 2 == 0;
+
+console.log(`${n} is ${isEven ? 'Even' : 'Odd'}`);
---
+++ Exercise03-DataTypes/Hristomir-Hristov/DTYPES02-005.js
@@ -0,0 +1,17 @@
+/*
+ Perform the actions with each of the arithmetic operations ( + - * / ) with Number and BigInt variables, 
+ save results in different variables and print them on the console. 
+*/
+
+let a = 2;
+let b = 3n;
+
+let xAdd = a + Number(b);
+let xSub = a - Number(b);
+let xMul = a * Number(b);
+let xDiv = a / Number(b);
+
+console.log(xAdd);
+console.log(xSub);
+console.log(xMul);
+console.log(xDiv);
