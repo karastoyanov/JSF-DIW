3687561891e48092068b7635e0c887e9e87df406
DimitarTom <167531147+DimitarTom@users.noreply.github.com>
2024-06-07 14:28:51 +0200

    Datatype02-003

---
+++ Exercise02-Basics/Dimitar-Tomov/Datatype02-003.js
@@ -0,0 +1,7 @@
+/*Make a program to add one variable of type Number and one variable of type BigInt.
+Print the type of the result and the result on the console.*/
+
+let num1 = 9;
+let bigIntnum = 1n;
+let result = BigInt(num1) + bigIntnum;
+console.log(result);
