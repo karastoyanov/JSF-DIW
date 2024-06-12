e31e849b96de193a809dbae8de21c1ab8a63b389
DimitarTom <167531147+DimitarTom@users.noreply.github.com>
2024-06-12 12:17:33 +0200

    datatype02-004

---
+++ Exercise03-DataTypes/Dimitar-Tomov/Datatype02-004.js
@@ -0,0 +1,9 @@
+/*Write a program in JavaScript to check if a given number is even or odd. 
+// use a ternary operator and string template*/
+
+let number1 = 42;
+let number2 = 17;
+
+let isEven = number1 % 2 == 0;
+
+console.log(`${number1} is ${isEven ? 'Even' : 'Odd'}`);
