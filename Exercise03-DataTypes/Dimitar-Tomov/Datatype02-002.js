00760a1ac023ebba474a471653ad498bfb7790df
DimitarTom <167531147+DimitarTom@users.noreply.github.com>
2024-06-12 11:44:21 +0200

    datatype02-01

---
+++ Exercise03-DataTypes/Dimitar-Tomov/Datatype02-002.js
@@ -0,0 +1,6 @@
+/*Try to re-write the following fragment of code: 
+JSF-Part1-Exercises  page: 11/11 
+let x = 0.12 + 0.21; 
+console.log(x);   //x = 0.32999999999999996  
+to calculate x properly.  
+HINT: work with integer numbers and then use the right number of decimals*/
