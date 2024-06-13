0a3e018ef8e811ef2eb8acd73872ac8448fe1a37
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-06-13 09:23:50 +0300

    DTYPES02-001

---
+++ Exercise03-Data Types/DTYPES02-001.js
@@ -0,0 +1,19 @@
+/* Define two number variables without decimal, two with decimal, two number
+variables in exponential notation; two variables in exponential notation (but with 4 digits
+for the whole part and 7 digits for the fractional part. */
+
+let num1 = 42;
+let num2 = 365;
+
+let num3 = 3.1;
+let num4 = 2.7;
+
+let num5 = 1.23e4; 
+let num6 = 5.67e3; 
+
+let num7 = 1234.5678901;
+let num8 = 5678.1234567;  
+
+
+
+
