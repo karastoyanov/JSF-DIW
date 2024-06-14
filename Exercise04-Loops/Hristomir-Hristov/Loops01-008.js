59d700aed87fe40d7950a351a4a80930d9adafdc
hrhristoff <hristomir.hristoff@gmail.com>
2024-06-14 17:19:07 +0300

    Modify task 8

--- Exercise04-Loops/Hristomir-Hristov/Loops01-008.js
+++ Exercise04-Loops/Hristomir-Hristov/Loops01-008.js
@@ -2,14 +2,14 @@
 Write a program to calculate the mathematical function factorial of a given number n.
 */
 
-let num = 5;
+let num = -2;
 
 if (num < 0) {
     console.log(`The number must be positive!`);
 } else {
-let result = 1;
-for (let i = 1; i <= num; i++) {
-    result *= i;
-}
-console.log(result);
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    console.log(result);
 }
