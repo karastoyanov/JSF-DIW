02004325bed2ebce18d332add48efea335f8a7dd
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-06-14 08:43:10 +0300

    DTYPES02-004

---
+++ Exercise03-Data Types/DTYPES02-004.js
@@ -0,0 +1,10 @@
+/*
+ Write a program in JavaScript to check if a given number is even or odd. // use a ternary operator and string template
+ */
+
+let n = 22;
+let isEven = n % 2 == 0;
+
+console.log(`${n} is ${isEven ? 'Even' : 'Odd'}`);
+
+
