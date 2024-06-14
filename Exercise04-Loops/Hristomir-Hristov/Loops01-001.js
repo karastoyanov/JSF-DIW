4f51e8b0c2d0725b58eac6518006e052bbffdeb2
hrhristoff <hristomir.hristoff@gmail.com>
2024-06-14 17:15:30 +0300

    Hristomir loops 1-8

---
+++ Exercise04-Loops/Hristomir-Hristov/Loops01-001.js
@@ -0,0 +1,14 @@
+/*
+Write a program that determines whether a given number is positive or
+negative.
+*/
+
+let n = 5;
+
+if (n < 0) {
+    console.log(`${n} is negative.`);
+} else if (n > 0) {
+    console.log(`${n} is positive.`);
+} else {
+    console.log(`${n} is 0.`);
+}
---
+++ Exercise04-Loops/Hristomir-Hristov/Loops01-008.js
@@ -0,0 +1,15 @@
+/*
+Write a program to calculate the mathematical function factorial of a given number n.
+*/
+
+let num = 5;
+
+if (num < 0) {
+    console.log(`The number must be positive!`);
+} else {
+let result = 1;
+for (let i = 1; i <= num; i++) {
+    result *= i;
+}
+console.log(result);
+}
