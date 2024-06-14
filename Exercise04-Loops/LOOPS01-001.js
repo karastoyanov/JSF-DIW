24888de60650988c88a84a4e5a63b526de46b220
pdilkinski <plamen.dilkinski@gmail.com>
2024-06-14 21:28:23 +0300

    adding solutions of tasks in loops

---
+++ Exercise04-Loops/LOOPS01-001.js
@@ -0,0 +1,22 @@
+let a = -5;
+
+if (a > 0)
+    {console.log(`${a} is a positive number`)}
+else 
+console.log(`${a} is a negative number`);
+
+
+/*let num = -10;
+
+switch (true) {
+    case (num > 0):
+    console.log('positive number');
+    break;
+    case(num < 0):
+    console.log('negative number');
+    break;
+    default:
+    console.log('number is zero');
+    break;
+} */
+  
---
+++ Exercise04-Loops/LOOPS01-003.js
@@ -0,0 +1,19 @@
+/* LOOPS01-003: There are given two variables initialized with numbers. Write a program to
+determine which one is the greater.
+*/
+
+let num1 = 13;
+let num2 = 33;
+
+if (num1 < num2)
+{
+    console.log(`${num1} is lower than ${num2}`);
+} 
+else  if (num1 > num2)
+{
+   console.log(`${num1} is bigger than ${num2}`)
+} 
+else 
+{   
+    console.log(`${num1} is equal to ${num2}`);
+}
