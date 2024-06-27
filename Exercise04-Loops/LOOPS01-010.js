b924a5764310f6a1918c1465adb99e696e06a367
pdilkinski <plamen.dilkinski@gmail.com>
2024-06-27 16:45:10 +0300

    solutions of loops tasks

--- Exercise04-Loops/LOOPS01-010.js
+++ Exercise04-Loops/LOOPS01-010.js
@@ -1,8 +1,17 @@
 // LOOPS01-010: Write a program to check if a given number is prime (divisible only by 1 and itself).
 
-let givenNum = 11;
+let givenNum = 90;
 
-for (let i = 2; i < givenNum; i++) {
+if(givenNum <= 1){
+    console.log(`${givenNum} is lower than or equal to 1, not a valid input!`)
+}else if(givenNum <=3){
+    console.log(`${givenNum} is prime number!`)
+}else if(givenNum % 2 == 0 || givenNum % 3 == 0){
+    console.log(`${givenNum} is not a prime number!`)
+}else{
+    console.log(`${givenNum} is a prime number!`)
+}
+/*for (let i = 2; i < givenNum; i++) {
     if (givenNum % i == 0) {
 
         console.log(`the number ${givenNum} is not prime number!`);
@@ -10,4 +19,4 @@ for (let i = 2; i < givenNum; i++) {
 
         console.log(`${givenNum} is prime number!`)
     }
-}
+}*/
