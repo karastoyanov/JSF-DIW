e64ee7fb5d3b1fb632823cd9c89c1b09fc74678e
SvetlinDIW <svetlin.trayanski@doitwise.com>
2024-06-27 10:40:35 +0300

    last two

---
+++ Exercise03-Basics/Svetlin-Trayanski/BASICS03-021.js
@@ -0,0 +1,14 @@
+/*Are there any not correctly defined expressions in the following excerpt? If
+any – which one(s)? Why?
+let a = 1; // correct
+let b = a; // correct 
+let r1 = a+--b; // correct 
+let r2 = a+++b; // incorrect, the correct expression should be a++ + b
+let r3 = a---b; // incorrect, the correct expression should be a-- - b
+let r4 = a-++b; // correct
+let r5 = a*++b; // correct
+let r6 = a*--b; // correct 
+let r7 = a**++b*b; // correct
+let r8 = a**++b //b; // correct
+
+*/ 
