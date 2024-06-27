c416e52ecbf35623b3bdc331e0db4e5cdbc7e477
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-06-27 14:58:44 +0300

    Quck fox!

---
+++ Exercise03-DataTypes/Ivan-Ivanov/DTYPES01-001.js
@@ -0,0 +1,11 @@
+/*
+DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use
+diWerent quotes).
+*/
+
+let firstName = "Ivan";
+let lastName = 'Ivanov';
+
+console.log("My name is " + firstName + " " + lastName);
+
+console.log(`My name is ${firstName} ${lastName}`);
