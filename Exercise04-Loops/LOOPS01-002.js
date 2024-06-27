896dc170f383a34f717c5d8c1f9cff862536766f
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-06-27 16:17:06 +0300

    LOOPS01-002

---
+++ Exercise04-Loops/LOOPS01-002.js
@@ -0,0 +1,9 @@
+/* Write a program that determines if a given number is even or odd. */
+
+let num = 7;
+
+if (num % 2 == 0) {
+    console.log(`Number ${num} is even.`);
+} else {
+    console.log(`Number ${num} is odd.`);
+}
