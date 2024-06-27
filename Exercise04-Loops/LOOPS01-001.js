5d2a12b2d837665772fd61da35e2158fc4b13647
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-06-27 16:13:45 +0300

    LOOPS01-001

--- Exercise04-Loops/LOOPS01-001.js
+++ Exercise04-Loops/LOOPS01-001.js
@@ -1,3 +1,12 @@
+/* Write a program that determines whether a given number is positive or
+negative. */
 
+let num = 7;
 
-
+if (num < 0) {
+    console.log(`Number ${num} is negative.`);
+} else if (num > 0) {
+    console.log(`Number ${num} is positive.`);
+} else {
+    console.log(`Number is 0.`);
+}
