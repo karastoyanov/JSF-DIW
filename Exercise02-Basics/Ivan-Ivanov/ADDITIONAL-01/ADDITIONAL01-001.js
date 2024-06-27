651711ad921ba865f522cfa34c199abdbd96134a
Ivan Ivanov <ivan.ivanov@doitwise.com>
2024-06-27 13:20:15 +0300

    Running late

---
+++ Exercise02-Basics/Ivan-Ivanov/ADDITIONAL-01/ADDITIONAL01-001.js
@@ -0,0 +1,22 @@
+let book1 = "Book title 1";
+let book2 = "Book title 2";
+let book3 = "Book title 3";
+let book4 = "Book title 4";
+let book5 = "Book title 5";
+let book6 = "Book title 6";
+let book7 = "Book title 7";
+let book8 = "Book title 8";
+let book9 = "Book title 9";
+let book10 = "Book title 10";
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
+console.log(book4);
+console.log(book5);
+console.log(book6);
+console.log(book7);
+console.log(book8);
+console.log(book9);
+console.log(book10);
+
