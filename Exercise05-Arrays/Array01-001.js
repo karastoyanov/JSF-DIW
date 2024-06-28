41fef75dd02ac2f811a630cdd34f6b9c27f8120b
pdilkinski <plamen.dilkinski@gmail.com>
2024-06-28 15:30:08 +0300

    first tasks from Arrays

---
+++ Exercise05-Arrays/Array01-001.js
@@ -0,0 +1,14 @@
+// Write a program to find the minimum element from one-dimensional array
+
+const myArray = [-100,1, 5, 15,-20,];
+let minEl = myArray[0];
+
+for ( let i = 0; i<myArray.length; i++){
+
+    if(myArray[i] < minEl){
+
+        minEl = myArray[i];
+
+    }
+    
+} console.log(`the min. element is ${minEl}`);
---
+++ Exercise05-Arrays/Array01-002.js
@@ -0,0 +1,14 @@
+// Write a program to find the max. element from one-dimensional array
+
+const myArray = [0,100,1, 5, 15,-20,];
+let maxEl = myArray[0];
+
+for ( let i = 0; i<myArray.length; i++){
+
+    if(myArray[i] > maxEl){
+
+        maxEl = myArray[i];
+
+    }
+    
+} console.log(`the max. element is ${maxEl}`);
