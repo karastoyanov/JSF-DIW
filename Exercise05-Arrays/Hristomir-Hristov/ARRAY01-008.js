2bfcc87b0506b1c0df2bee17703489da70d61ecd
hrhristoff <hristomir.hristoff@gmail.com>
2024-06-28 16:11:57 +0300

    Arrays exs Hristomir

---
+++ Exercise05-Arrays/Hristomir-Hristov/ARRAY01-008.js
@@ -0,0 +1,10 @@
+/*
+Write a program to create and print one-dimensional array in reverse order.
+*/
+
+const sequence = [-5, 112, -223, -55, 88, 293];
+
+for (let i = sequence.length-1; i >= 0; i--) {
+
+    console.log(sequence[i]);
+}
