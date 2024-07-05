d852d9254148255a22f8077daf5e39e224d85921
pdilkinski <plamen.dilkinski@gmail.com>
2024-07-05 15:00:09 +0300

    first taks functions

---
+++ Exercise06-Functions/Functions01-001.js
@@ -0,0 +1,12 @@
+// FUNCTIONS01-001: Write a function that takes two numbers as arguments and returns the largest of them
+
+function myFunction(a, b) {
+    if (a > b) {
+        return a;
+    } else if (b > a) {
+        return b;
+    } else {
+        return [a, b];
+    }
+} 
+console.log(myFunction(0,3));
