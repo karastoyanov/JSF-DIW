ee515c91653a90bf9480c3988f97af6f52361442
pdilkinski <plamen.dilkinski@gmail.com>
2024-07-12 14:37:43 +0300

    commit of task that is still pending update

---
+++ Exercise06-Functions/Functions01-005.js
@@ -0,0 +1,12 @@
+//  FUNCTIONS01-005: Write a function that takes a number as an argument and returns the sum of its digits.
+
+function sumOfNumber(a){
+var numConvert = a.toString().split(' ');
+let sum = 0;
+    for (let i = 0; i<numConvert.length;i++){
+         numConvert[i];
+         console.log(numConvert[i]);
+
+    } 
+
+} console.log(sumOfNumber(2445));
