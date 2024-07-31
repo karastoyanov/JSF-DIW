236467147284829c96d831d161a28c79c6d72b17
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-07-31 16:30:04 +0300

    ARRAY-001

---
+++ Exercise05-Arrays/Alex-Pilarski/ARRAY01-001.js
@@ -0,0 +1,13 @@
+/* Write a program to find the minimum element from a one-dimensional array.*/
+
+const array = [3, 1, 4, 8, 5, 9, 2, 6, 5, 3, 5];
+
+
+let minElement = array[0];
+
+for (let i = 1; i < array.length; i++) {
+    if (array[i] < minElement) {
+        minElement = array[i];
+    }
+}    
+console.log(`The minimum element in the array is: ${minElement}`);
