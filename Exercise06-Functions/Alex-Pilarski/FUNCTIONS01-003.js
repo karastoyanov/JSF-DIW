a416a3632799959abeb11c76fa6f821d23bafd0a
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-08-01 08:55:23 +0300

    FUNCTIONS01-003

---
+++ Exercise06-Functions/Alex-Pilarski/FUNCTIONS01-003.js
@@ -0,0 +1,21 @@
+/*
+Write a function that takes a number as an argument and returns:
+• “Fizz” if the number is divisible by 3
+• “Buzz” if the number is divisible by 5
+• “FizzBuzz” if the number is divisible by both 3 and 5
+• The number itself if it is divisible by neither
+*/
+
+function fizzBuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "FizzBuzz";
+    } else if (number % 3 === 0) {
+        return "Fizz";
+    } else if (number % 5 === 0) {
+        return "Buzz";
+    } else {
+        return number;
+    }
+}
+
+console.log(fizzBuzz(10));
