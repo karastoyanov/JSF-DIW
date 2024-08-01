20aec9828ecdda1700bea7cc8935685aa315fa7e
alexpilarski on Mac OS <alexander_pilarski@yahoo.com>
2024-08-01 08:52:37 +0300

    FUNCTIONS01-002

---
+++ Exercise06-Functions/Alex-Pilarski/FUNCTIONS01-002.js
@@ -0,0 +1,10 @@
+/* Write a function that takes a single number as an argument and returns true if the number is even, and false otherwise. */
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+console.log(evenOrOdd(7)); 
