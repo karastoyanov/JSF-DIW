f7a8bb157bf332ff88152cbe037c58b23cf9fb12
Stoian02 <stoianstoikov@gmail.com>
2025-11-11 16:53:54 +0200

    Add: DTYPES01-001 exercise

---
+++ Exercise03-DataTypes/Stoyan-Stoykov/Strings/DTYPES01-001.js
@@ -0,0 +1,8 @@
+/**
+ * DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use different quotes).
+ */
+
+let first = 'Stoyan Stoykov';
+let second = "Stoyan Stoykov";
+
+console.log(first === second); // should be true
