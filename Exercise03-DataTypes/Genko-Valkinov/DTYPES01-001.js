ed16d26e8aba321f1da49280354e4a19197edfdd
Genko Valkinov <gvalkinov@gmail.com>
2025-11-11 16:54:02 +0200

    Added DTypes01-001 to 006 exercises

---
+++ Exercise03-DataTypes/Genko-Valkinov/DTYPES01-001.js
@@ -0,0 +1,5 @@
+// DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use 
+// different quotes).
+
+let name1 = 'Genko Valkinov';
+let name2 = "Genko Valkinov";
