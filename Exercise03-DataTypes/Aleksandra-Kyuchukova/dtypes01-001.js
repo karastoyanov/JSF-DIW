21898474bc39ef4c22753cb6a85e90284a0509e2
Aleksandra Kyuchukova <aleksandra.kyuchukova@inetum.com>
2025-11-11 17:07:53 +0200

    Added exercise dtypes01-001

---
+++ Exercise03-DataTypes/Aleksandra-Kyuchukova/dtypes01-001.js
@@ -0,0 +1,8 @@
+/*DTYPES01-001: Define a string variable with your names (first and last) in two ways (hint: use 
+different quotes). */
+
+let name = 'Aleksandra Kyuchukova';
+
+let nameV2 = "Aleks Kyuchukova";
+
+console.log(nameV2);
