014b2dccc47d67d9b222716a330415e2db9bc2fc
ivaylochenchev <58465111+ivaylochenchev@users.noreply.github.com>
2025-11-14 13:23:33 +0200

    Update BASICS01-001.js
    Constants must be with capital letters (the whole identifier)

--- Exercise02-Basics/Plamen-Marinov/BASICS01/BASICS01-001.js
+++ Exercise02-Basics/Plamen-Marinov/BASICS01/BASICS01-001.js
@@ -5,8 +5,8 @@ BASICS01-001: Write ten correct identifiers, following the camelCase naming conv
 let userName = "plam";
 let firstName = "Plamen";
 let lastName = "Marinov";
-const birthYear = 1981;
-const phoneNumber = "+359887453153";
+const birthYear = 1981;                    // constants must be with CAPITAL letters -> const BIRTH_YEAR = 1981;
+const phoneNumber = "+359887453153";       // const PHONE_NUMBER = "+359887453153";
 let productName = "Laptop";
 let pproductNumber = "123456";
 let serialNumber = "SN123456789";
